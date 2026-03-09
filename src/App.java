3fdc19629ff30b45237f805fb11ab19b424e7bf7
Fernando Marrero <fernando.marrero@salesianos-lacuesta.net>
2026-03-09 18:27:24 +0000

    prueba pull comandos y comprobamos si el producto es apto

--- src/App.java
+++ src/App.java
@@ -6,5 +6,11 @@ public class App {
       Producto p1 = new Producto("Manzana", "2024-12-31", 95);
         //mostramos su informacion
       p1.mostrarInformacion();
+        //comprobamos si es apto para dieta
+        if (p1.esAptoParaDieta()) {
+            System.out.println("El producto es apto para dieta.");
+        } else {
+            System.out.println("El producto no es apto para dieta.");
+        }
     } 
 }

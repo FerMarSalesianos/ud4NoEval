ffe427190dec2f88c598faebcbf98b305c3d9677
Fernando Marrero <fernando.marrero@salesianos-lacuesta.net>
2026-03-09 18:20:59 +0000

    Creamos productos en main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import net.salesianos.productos.Producto;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola clase de programación!");
-    } //primera prueba del proyecto
+      //creamos un producto
+      Producto p1 = new Producto("Manzana", "2024-12-31", 95);
+        //mostramos su informacion
+      p1.mostrarInformacion();
+    } 
 }
